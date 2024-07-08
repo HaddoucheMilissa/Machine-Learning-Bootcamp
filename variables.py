604f863d6e6562294b7367729887783d9e25e36d
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-08 16:39:21 +0100

    different operations

---
+++ variables.py
@@ -0,0 +1,25 @@
+#variables
+x=2 #int
+y=3 # int
+a=True #bool
+b= False #bool
+z=4.5 #float
+w="machine learning" #char
+
+#arithmetique 
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y) #power X^y
+
+#logique 
+print(a|b) #OR
+print(a&b) #AND
+print(a^b) #XOR
+
+#comparaison
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
