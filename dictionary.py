1a839addbe56b6e75f14a0e99d8295e6ec9a2500
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-09 17:49:18 +0100

    intro to dict

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+dict1={"Alger":16,
+       "Blida":9,
+       "Adrar":1,
+       "Bejaia":6
+       }
+print(dict1)
+
+#in deep learning
+import numpy as np 
+settings={
+    "W1": np.random.randn(1,20),
+    "b1":np.random.rand(13,16),
+    "W1":np.random.rand(20,1),
+    "b2":np.random.rand(1,16),
+}
