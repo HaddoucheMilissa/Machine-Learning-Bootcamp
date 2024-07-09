4205a27d416a3d9f24c5293668a299c1b954eb85
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-09 20:50:51 +0100

    get and fromkeys method

--- dictionary.py
+++ dictionary.py
@@ -13,3 +13,20 @@ settings={
     "W1":np.random.rand(20,1),
     "b2":np.random.rand(1,16),
 }
+
+#to get the values 
+print(dict1.values())
+#to get the keys
+print(dict1.keys())
+#to get the lenght
+print("the lenght of my dict is:",len(dict1))
+# to add an element 
+dict1["tizi-ouzou"]=15
+print(dict1)
+#get method
+print(dict1.get("constantine")) #the value doesnt exist
+print(dict1.get("Alger")) #output 16
+
+#fromkeys method
+list1=("banana","water","tomato","kiwi","oil")
+print(dict1.fromkeys(list1,"test"))
