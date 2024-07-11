5b2d3b7a57bcef355f1c94ba61fb5df21931aa8d
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-11 19:54:52 +0100

    some built functions

---
+++ builtinfunctions.py
@@ -0,0 +1,19 @@
+#abs
+x=-2
+print(abs(x))
+x=7
+print(abs(x))
+#round
+x=8.65
+print(round(x))
+x=3.12
+print(round(x))
+#max
+l=[11,9,0,43,12,99,38]
+print("the max is",max(l))
+print("the min is",min(l))
+#len
+print(len(l))
+#sum
+print(sum(l)) #11+9+0+43+12+99+38
+
