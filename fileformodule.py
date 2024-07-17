1fb00c75908820bc9192d504b0c3f4d81e151898
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-17 23:15:32 +0100

    file connected to module_file

---
+++ fileformodule.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    a,b=0,1
+    fibo=[]
+    while a<n:
+        fibo.append(a)
+        a,b=b,a+b
+    return fibo
+
+def area (h,b,a):
+    a=2
+    while b>0 & h>0:
+        x=(h*b)/2
+    return x
