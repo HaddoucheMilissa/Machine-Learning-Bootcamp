8e132a781969cae6007336d88d48ea837a47f722
HaddoucheMilissa <melissahaddo18@gmail.com>
2024-07-18 00:02:48 +0100

    somemath and stats modules

--- module.py
+++ module.py
@@ -10,3 +10,22 @@ from fileformodule import fibonacci
 liste=fibonacci(40)
 print(liste)
 
+# modules
+import math
+import statistics
+import random
+import os
+import glob
+
+# MATH
+print(math.pi)
+print(math.cos(0))
+print(math.cos(2*math.pi))
+print(math.exp(5))
+print(math.log10(30))
+
+# STATS
+print(statistics.mean(liste))
+print(statistics.variance(liste))
+print(statistics.median(liste))
+
